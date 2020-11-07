ba441a1f19f10a7b709f7f218800f318ea635a2d
habibi <habibijaelani05@gmail.com>
2020-11-07 10:20:11 +0000

    Add Comment

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 # DDP LAB-4
-# Nama: <Tulis nama di sini>
-# NIM: <Tulis NIM di sini>
+# Nama: Habibi
+# NIM: 0110220247
 
 # SOAL 1 - Mencetak nama
 # Tuliskan program untuk Soal 1 di bawah ini
-nama = input("Masukkan nama: ")
-lenNama = len(nama)
-iNama = 1
-while iNama <= lenNama:
-  print(nama[0:iNama])
-  iNama = iNama + 1
 
+# Simpan masukan nama kedalam variabel nama
+nama = input("Masukkan nama: ")
+# Simpan panjang nama kedalam variabel lenNama
+lenNama = len(nama)
+# Deklarasi variabel iNama dengan nilai 1 untuk kebutuhan perulangan
+iNama = 1
+# Lakukan perulangan sebanyak nilai yang ada di variabel lenNama
+while iNama <= lenNama:
+    # print nama dengan String Slicing sesuai urutan perulangan
+    print(nama[0:iNama])
+    # tambahkan nilai iNama dengan 1
+    iNama = iNama + 1
 
 
 # SOAL 2 - Validasi teks
 # Tuliskan program untuk Soal 2 di bawah ini
+
+# Simpan masukan text kedalam variabel text
 text = input("\nMasukkan text: ")
 
+# Program melakukan pengecekan apakah panjang text lebih atau sama dengan 8, terdapat kata nf, terdapat YYY atau yyy, dan terdapat angka di dalam text tersebut
 if len(text) >= 8 and ( 'nf' in text.lower()) and (text.endswith('YYY') or text.endswith('yyy')) and any(char.isdigit() for char in text):
-  print("Teks valid. Program berhenti.")
+    # jika memenuhi syarat maka print
+    print("Teks valid. Program berhenti.")
 else:
-  print("Teks tidak valid.")
+    # jika tidak memenuhi syarat maka print
+    print("Teks tidak valid.")
   
